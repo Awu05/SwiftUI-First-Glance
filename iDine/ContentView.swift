79c5bda222aae32c9c96be8fc52a123cbb2d4e86
Andy Wu <andywu.05@gmail.com>
2019-10-06 18:22:42 -0400

    Initial Commit

---
+++ iDine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iDine
+//
+//  Created by Andy Wu on 10/6/19.
+//  Copyright © 2019 Andy Wu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
